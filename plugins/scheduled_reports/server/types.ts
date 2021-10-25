bf9de970d0a2e5f5faaae8c91adb138a41bef68e
alikhadour <91189080+alikhadour@users.noreply.github.com>
2021-10-25 15:45:38 +0300

    Delete plugins/scheduled_reports directory

--- plugins/scheduled_reports/server/types.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ScheduledReportsPluginSetup {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ScheduledReportsPluginStart {}
